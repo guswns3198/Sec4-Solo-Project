5f6c732fe66a5cbca55c680df7eb33b3b331af23
guswns3198 <guswns3198@naver.com>
2022-08-17 01:57:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import React from 'react';
+import Image from '../src/images/webImage';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
+  .App {
+    background-image: "https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_1280.jpg";
+  }
+`;
+
+
+const Clock = () => {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+}
+
+Clock();
+setInterval(Clock, 1000);
 
 function App() {
   return (
+    <Wrapper>
+        <img src={Image} alt="배경" className="background"/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <span>동해물과 백두산이</span>
     </div>
+    </Wrapper>
   );
 }
 
